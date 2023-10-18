628c56e351104276b544728c12abac12959e98ff
Manuel <Manuel>
2023-10-18 11:35:11 +0100

    Cambios en la pantalla juego

--- Juego de memoria/JuegoViewController.swift
+++ Juego de memoria/JuegoViewController.swift
@@ -11,5 +11,46 @@ import UIKit
 class JuegoViewController:  UIViewController {
     
     
+    var images = [""]
+    var imagesCorrectas = [""]
+    
+    
+    @IBOutlet weak var RandomImageShown: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+       showImages()
+        
+        
+        
+        
+       
+        
+    }
+    
+    
+    func showImages(){
+        
+        let count = 5
+        
+        for i in 0 ... count {
+            
+            imagesCorrectas.insert("", at: i)
+            
+            
+        }
+        
+        
+        let url = URL(string: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" )
+        let data = try? Data(contentsOf: url!)
+        RandomImageShown.image = UIImage(data: data!)
+        
+        
+    }
+    
+    
     
 }
