2376eb215ee6c78c7ac765fc744688f265934748
Manuel <Manuel>
2023-11-02 13:17:38 +0000

    Mejoras en collectionView

--- Juego de memoria/ResolverJuegoViewController.swift
+++ Juego de memoria/ResolverJuegoViewController.swift
@@ -9,18 +9,32 @@ class ResolverJuegoViewController: UIViewController,  UICollectionViewDataSource
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! imageCollectionViewCell
-        
         let url:URL? = URL(string: images[indexPath.row])
         let data = try? Data(contentsOf: url!)
         
-        
         cell.imageView.image = UIImage(data: data!)
-        
         
         return cell
     }
-    
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let imgWidth = view.bounds.width
+        let imgHeight = imgWidth
+
+        return CGSize(width: imgWidth, height: imgHeight)
+    }
     
     var puntuacion = 0
     @IBOutlet weak var PuntuacionText: UILabel!
